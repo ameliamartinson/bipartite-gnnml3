66a34bf0a1c0ab8a133d9f5a39115ba3f7b71e4b
Amelia Martinson <ameliamartinson01@gmail.com>
2026-04-27 15:14:22 -0700

    update stuff

--- spectral.py
+++ spectral.py
@@ -45,29 +45,38 @@ def spectral_design_trunc(G: nx.Graph,M: sp.csr_matrix, s,k,phi,h,g): #
     B = normalized_biadjacency(G)
     m,n = B.shape
 
-    M = M
-    M_11 = M[0:m,0:m]
-    M_12 = M[0:m, m:]
-    M_21 = M[m:, 0:m]
-    M_22 = M[m:, m:]
+    M_11 = M[0:m,0:m].tocoo()
+    M_12 = M[0:m, m:].tocoo()
+    M_21 = M[m:, 0:m].tocoo()
+    M_22 = M[m:, m:].tocoo()
 
-    U,S,Vt = sp.linalg.svds(B, k=6)
-    lambda_max = S[0]    
+    U,S,Vt = sp.linalg.svds(B, k=k)
+    V = Vt.T
+    lambda_max = S.max()
 
     s_vals = np.linspace(-lambda_max, lambda_max, s)
-    print(s_vals)
-    #h_S = np.diag(h(S))
-    #g_S = np.diag(g(S))
+    print("s_vals: ", s_vals)
+
+    def compute_sparse_block(Mask_coo, Left_emb, Right_emb, filter_S):
+        r = Mask_coo.row
+        c = Mask_coo.col
+
+        if len(r) == 0:
+            return sp.coo_matrix(Mask_coo.shape)
+
+        vals = np.sum(Left_emb[r,:] * filter_S * Right_emb[c, :], axis=1)
+
+        return sp.coo_matrix((vals, (r,c)), shape=Mask_coo.shape)
 
     ret_arr = []
     for f_s in s_vals:
-        h_S = np.diag(h(S, f_s=f_s))
-        g_S = np.diag(g(S, f_s=f_s))
+        h_S = h(S, f_s=f_s)
+        g_S = g(S, f_s=f_s)
 
-        UU = M_11*(U@h_S@U.T)
-        UV = M_12*(U@g_S@Vt)
-        VU = M_21*(Vt.T@g_S@U.T)
-        VV = M_22*(Vt.T@h_S@Vt) 
+        UU = compute_sparse_block(M_11, U, U, h_S)
+        UV = compute_sparse_block(M_12, U, V, g_S)
+        VU = compute_sparse_block(M_21, V, U, g_S)
+        VV = compute_sparse_block(M_22, V, V, h_S)
 
         ret_arr.append(sp.bmat([[UU,UV],[VU,VV]]))
 
